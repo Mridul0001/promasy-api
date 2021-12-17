f366176f399edc1bfc01220f837f5ddc62c195d6
Mridul0001 <mridulmahajan0001@gmail.com>
2021-12-17 20:56:25 +0530

    cors issue resolved

--- src/main/java/com/promasy/api/PromasyApiApplication.java
+++ src/main/java/com/promasy/api/PromasyApiApplication.java
@@ -2,15 +2,28 @@ package com.promasy.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableMongoRepositories
 public class PromasyApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PromasyApiApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("https://promasy-ui.herokuapp.com");
+			}
+		};
 	}
 }
